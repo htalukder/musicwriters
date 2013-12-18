23124a63f4b12a9d338bad964a8cc959aa81a5c0
Hisham <htalukder@math.umd.edu>
2013-12-17 20:46:16 -0500

    Including data in git repo

---
+++ Data/graph.js
@@ -0,0 +1,86 @@
+<style>
+
+.node {
+  stroke: #fff;
+  stroke-width: 1 px;
+}
+
+.link {
+  stroke: #999;
+  stroke-opacity: .6;
+}
+
+</style>
+
+<script src="http://d3js.org/d3.v2.js"></script>
+<script type="text/javascript">var networkOutputBinding = new Shiny.OutputBinding();
+  $.extend(networkOutputBinding, {
+    find: function(scope) {
+      return $(scope).find('.shiny-network-output');
+    },
+    renderValue: function(el, data) {
+      
+      //format nodes object
+      var nodes = new Array();
+      for (var i = 0; i < data.names.length; i++){
+        nodes.push({"name": data.names[i]})
+      }
+
+
+      var width = 600;
+      var height = 430;
+    
+      var lin = data.links
+      var force = d3.layout.force()
+        .nodes(nodes)
+        .links(lin)
+        .charge(-120)
+        .linkDistance(30)        
+        .size([width, height])
+        .start();
+      
+      //remove the old graph
+      var svg = d3.select(el).select("svg");      
+      svg.remove();
+      
+      $(el).html("");
+      
+      //append a new one
+      svg = d3.select(el).append("svg");
+      
+      svg.attr("width", width)
+        .attr("height", height);
+    
+      var link = svg.selectAll("line.link")
+          .data(lin)
+        .enter().append("line")
+          .attr("class", "link")
+          .style("stroke-width", function(d) { return Math.sqrt(d.value); });
+    
+      var node = svg.selectAll("circle.node")
+          .data(nodes)
+        .enter().append("circle")
+          .attr("class", "node")
+          .attr("r", 5)
+          //.style("fill", function(d) { return color(d.group); })
+          .call(force.drag);
+      node.append("title")
+        .text(function(d) { return d.name; });
+        
+        
+      var offset = 0;
+      force.on("tick", function() {
+        link.attr("x1", function(d) { return d.source.x - offset; })
+            .attr("y1", function(d) { return d.source.y - offset; })
+            .attr("x2", function(d) { return d.target.x - offset; })
+            .attr("y2", function(d) { return d.target.y - offset; });
+    
+        node.attr("cx", function(d) { return d.x - offset; })
+            .attr("cy", function(d) { return d.y - offset; });
+      });
+      
+    }
+  });
+  Shiny.outputBindings.register(networkOutputBinding, 'trestletech.networkbinding');
+  
+  </script>
