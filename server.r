f4e6bb74f6b388e237c85d32c6932d13e68e912b
nosson <jpaulson@umiacs.umd.edu>
2013-12-17 21:39:00 -0500

    Automatically load R package dependencies

--- server.r
+++ server.r
@@ -1,39 +1,33 @@
-library(shiny)
-library(ShinyDash)
-library(XML)
-library(httr)
-library(igraph)
-library(stringr)
-library(seqinr)
-library(googleVis)
-library(rCharts)
+download_not_installed<-function(x){
+	for(i in x){
+		if(!require(i,character.only=TRUE)){
+	  		install.packages(i,repos="http://cran.r-project.org")
+  			library(i,character.only=TRUE)
+  		}
+	}
+}
+required_packages = c("shiny","XML","httr","igraph","stringr","seqinr","googleVis","devtools")
+download_not_installed(required_packages)
+
+if(!(require("rCharts"))){
+	devtools::install_github('rCharts', 'ramnathv')
+}
+if(!(require("ShinyDash"))){
+	devtools::install_github("ShinyDash", "trestletech")
+}
 
 #load("AdjMat.rDa")
 #load("ind_deg.rDa")
-load("listmat.rDa")
-load("yearly_summary.rDa")
-load("AdjMatF.rDa")
-load("unSongMatList.rDa")
+load("./Data/listmat.rDa")
+load("./Data/yearly_summary.rDa")
+load("./Data/AdjMatF.rDa")
+load("./Data/unSongMatList.rDa")
 
 AdjMat=AdjMatF
 
 reactiveAdjacencyMatrix <- function(func){
   reactive(function(){
-    
 	val=func()
-    
-    #diag(val) <- 0
-    
-    #make the matrix symmetric
-    #val <- symmetricize(val, method="avg")
-    
-    #now consider only the upper half of the matrix
-    #val[lower.tri(val)] <- 0
-  
-    #mat=AdjMat[[input$Year-1969]]
-   # mat[lower.tri(mat,diag=TRUE)] = 0
-    #val = mat
-
     conns <- cbind(source=row(val)[val>0]-1, target=col(val)[val>0]-1, weight=val[val>0])
     rownames(val)=gsub( "([A-Z])", " \\1", rownames(val), perl=T)  
     list(names=rownames(val), links=conns)
@@ -41,27 +35,19 @@ reactiveAdjacencyMatrix <- function(func){
 
 }
 
-
 shinyServer(function(input, output) {
-	
-  
 	plotVal= reactive({  
-		
 		dat=unSongMatList[[input$Year-1969]][,c(1,2,5)]	
 		temp=dat[,3]
 		songL=rep(0, length(temp))
-		
 		for ( i in 1:length(temp)){
-		songU=as.character(temp[i])
-		songL[i]=paste('<iframe src="https://embed.spotify.com/?uri=',songU,'" width="250" height="80" frameborder="0" allowtransparency="true"></iframe>', sep="")
+			songU=as.character(temp[i])
+			songL[i]=paste('<iframe src="https://embed.spotify.com/?uri=',songU,'" width="250" height="80" frameborder="0" allowtransparency="true"></iframe>', sep="")
 		}
-		
 		unique_dat=cbind(dat[,1:2], songL)
 		unique_dat=data.frame(unique_dat)
 		colnames(unique_dat)=c("Songs", "Writers", "Spotify")
-
 		return (unique_dat)
-
 		})
 	
 	main=reactive({
@@ -75,31 +61,19 @@ shinyServer(function(input, output) {
 			{main="Time-Series of LSCC"}
 		return (main)
 	})
-		
-		
+
 	output$List= renderGvis({
 		   gvisTable(plotVal(), options = list(allowHTML = TRUE, height= "260px", 	width="587",
-		   													showRowNumber=TRUE))
-
+					showRowNumber=TRUE))
 		})
-	
-	
-	
-		
+
 	output$scatter= 
-	renderPlot({
-		plot(x=1970:2013, yearly_summary[,input$type], type="l", cex=.5, xlab="Year", ylab=input$type, 	ylim=c(0,round(max(yearly_summary[,input$type]), digits=0)), main=main())
-		points(x=input$Year, y=yearly_summary[input$Year-1969,input$type], col="red", bg="red", pch=21, cex=2)
-
-		})	
-		 
+		renderPlot({
+			plot(x=1970:2013, yearly_summary[,input$type], type="l", cex=.5, xlab="Year", ylab=input$type, 	ylim=c(0,round(max(yearly_summary[,input$type]), digits=0)), main=main())
+			points(x=input$Year, y=yearly_summary[input$Year-1969,input$type], col="red", bg="red", pch=21, cex=2)
+	})	 
     output$mainnet <- reactiveAdjacencyMatrix(function() {
-    
-    data=AdjMat[[input$Year-1969]]
-    
-
-	data
-  })
-
-
+    	data=AdjMat[[input$Year-1969]]
+    	data
+	})
 })
--- ui.r
+++ ui.r
@@ -1,70 +1,51 @@
-library(shiny)
-library(ShinyDash)
-require(devtools)
-library(rCharts)
+download_not_installed<-function(x){
+	for(i in x){
+		if(!require(i,character.only=TRUE)){
+	  		install.packages(i,repos="http://cran.r-project.org")
+  			library(i,character.only=TRUE)
+  		}
+	}
+}
+required_packages = c("shiny","devtools")
+download_not_installed(required_packages)
 
-reactiveNetwork <- function (outputId) 
-{
+if(!(require("rCharts"))){
+	devtools::install_github('rCharts', 'ramnathv')
+}
+if(!(require("ShinyDash"))){
+	devtools::install_github("ShinyDash", "trestletech")
+}
+
+reactiveNetwork <- function (outputId) {
   HTML(paste("<div id=\"", outputId, "\" class=\"shiny-network-output\"><svg /></div>", sep=""))
 }
 
 
 shinyUI(bootstrapPage(
-  h1("Writers of Number 1 Singles "),
-  
-  gridster(tile.width = 280, tile.height = 5,
-    
-	gridsterItem(col = 1, row = 1, size.x = 2, size.y = 3,
-                
-                sliderInput("Year", "Choose Year",
-                            min = 1970, max = 2013, value = 1970, format="####")
+	h1("Writers of Number 1 Singles "),
+	gridster(tile.width = 280, tile.height = 5,
+	    gridsterItem(col = 1, row = 1, size.x = 2, size.y = 3,
+					sliderInput("Year", "Choose Year",
+					min = 1970, max = 2013, value = 1970, format="####")
+					),
+		gridsterItem(col = 3, row = 1, size.x = 2, size.y = 9,
+			tags$head(tags$style(type="text/css", "select { height: 30px; width: 300px; fontSize:2}")),
+			selectInput("type", "Choose Descriptive Statistics:",
+					list("Average Degree" = "Degree Average",
+						"Clustering Coefficient" = "Clustering Coefficient",
+						"Largest Strongly Connected Component"= "LSCC",
+						"Network Density"= "Network Density (%)")),
+			plotOutput("scatter", height="230px")),
 
+		gridsterItem(col = 1, row = 4, size.x = 2, size.y = 12,
+			includeHTML("./Data/graph.js"),
+			reactiveNetwork(outputId = "mainnet")),
 
-    ),
-				
+	    gridsterItem(col = 3, row = 10, size.x = 2, size.y = 8,
+			htmlOutput("List")),
 
-	gridsterItem(col = 3, row = 1, size.x = 2, size.y = 9,
-				  tags$head(
-        		tags$style(type="text/css", "select { height: 30px; width: 300px; fontSize:2}")),
-
-				selectInput("type", "Choose Descriptive Statistics:",
-                 list("Average Degree" = "Degree Average",
-                      "Clustering Coefficient" = "Clustering Coefficient",
-                      "Largest Strongly Connected Component"= "LSCC",
-                      "Network Density"= "Network Density (%)")),
-
-
-                 plotOutput("scatter", height="230px")
-			
-
-)
-				
-    ,
-
-	gridsterItem(col = 1, row = 4, size.x = 2, size.y = 12,
-                  includeHTML("graph.js"),
-    			reactiveNetwork(outputId = "mainnet") 
-    		
-                
-    ),
-    gridsterItem(col = 3, row = 10, size.x = 2, size.y = 8,
-                  htmlOutput("List") 
-    
-    ),
-
-	gridsterItem(col = 1, row = 16, size.x = 2, size.y = 2,
-                  tags$p("",
-                  #tags$br(),
-                  tags$a(href = "https://github.com/htalukder/musicwriters", 				"Source Code", target="_blank")
-                ),
-		   tags$p("Copyright:",
-                  #tags$br(),
-                  tags$a(href = "http://cbcb.umd.edu/~htalukde", 				"Hisham Talukder", target="_blank")
-                )
-
-    )
-
-
-
-  )
+		gridsterItem(col = 1, row = 16, size.x = 2, size.y = 2,tags$p("",
+			tags$a(href = "https://github.com/htalukder/musicwriters", "Source Code", target="_blank")),
+			tags$p("Copyright:",tags$a(href = "http://cbcb.umd.edu/~htalukde", "Hisham Talukder", target="_blank")))
+	)
 ))
